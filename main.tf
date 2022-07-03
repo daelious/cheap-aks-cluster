afcc074b027ff00436ad22f9fa449163a881a4c4
Keith Meyer <meyer.keith@gmail.com>
2022-07-03 19:59:05 -0400

    Testing out k8s stuff.

--- main.tf
+++ main.tf
@@ -1,19 +1,29 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "daelious-k8s-rg"
   location = var.resource_group_location
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "daelious-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "daelious-k8s"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_B2s"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Sandbox"
+  }
 }
