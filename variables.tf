a433b5d2f6947ddc0f02de2684f76c40ecec6cfc
Keith Meyer <meyer.keith@gmail.com>
2022-07-03 20:19:22 -0400

    Update variables in workflow.

--- variables.tf
+++ variables.tf
@@ -7,3 +7,6 @@ variable "resource_group_name" {
   default = "daelious-k8s-rg"
   description   = "Name of the resource group."
 }
+
+variable "azure_spn_id" { description = "ID of the SPN for k8s." }
+variable "azure_spn_secret" { description = "ID of the SPN for k8s." }
