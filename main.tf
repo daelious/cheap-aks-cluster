a0c2bf3c7df18addc60366fc82b9adeb3665e21e
Keith Meyer <meyer.keith@gmail.com>
2022-07-03 20:05:15 -0400

    Try to fix error from workflow.

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "daelious-k8s-rg"
+  name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "daelious-aks"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.location
+  resource_group_name = azurerm_resource_group.name
   dns_prefix          = "daelious-k8s"
 
   default_node_pool {
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "resource_group_location" {
   default = "eastus2"
   description   = "Location of the resource group."
 }
+
+variable "resource_group_name" {
+  default = "daelious-k8s-rg"
+  description   = "Name of the resource group."
+}
